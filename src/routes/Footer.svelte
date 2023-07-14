d0210a994ff51a900c33d7969dc10b46895056a3
James Hinds <jahbini@icloud.com>
2023-07-14 15:51:43 -0400

    add left padding to Footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -7,9 +7,6 @@
 footer(class="footer")
   p
     | © 2016 James A. Hinds
-    br
-    | Powered by
-    a(href='https://github.com/jahbini/site-master', target='_blank') Site Master
 </template>
 
 <style>
@@ -26,6 +23,7 @@ footer(class="footer")
     width: 100%;
     background-color: beige;
     padding-top: 0.1in;
+    padding-left: 0.1in;
     bottom: 0px;
   }
 </style>
